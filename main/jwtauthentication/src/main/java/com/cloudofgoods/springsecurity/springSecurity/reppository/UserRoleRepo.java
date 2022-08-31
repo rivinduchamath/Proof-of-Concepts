1d7ac49fea9619e81f6c58700d2735e0fbfdfacf
Rivindu Wijayarathna <41599441+rivinduchamath@users.noreply.github.com>
2022-08-31 09:11:38 +0530

    Delete main directory

--- main/jwtauthentication/src/main/java/com/cloudofgoods/springsecurity/springSecurity/reppository/UserRoleRepo.java
+++
@@ -1,7 +0,0 @@
-package com.cloudofgoods.springsecurity.springSecurity.reppository;
-
-import com.cloudofgoods.springsecurity.springSecurity.model.UserRole;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRoleRepo extends JpaRepository<UserRole, Long> {
-}
